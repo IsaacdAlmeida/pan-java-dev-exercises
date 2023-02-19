2711212b08a55e62279d87de9e2fbe2908fb9ac4
Isaac Almeida <isaacalmeidafilho@gmail.com>
2023-02-19 15:03:49 -0300

    feat: operadores pt3

---
+++ dia_01_sintaxe_java/operadores/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+        numero = - numero;
+
+        // para tornar positivo devemos multiplicar o número por -1;
+
+        System.out.println(numero);
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+    }
+}
---
+++ dia_01_sintaxe_java/operadores/src/notes.md
@@ -0,0 +1,5 @@
+# Operadores
+
+## Operadores unários
+
+ 
